a8704bd39133181fbe6c8740008356d03c245da8
Ed <edmund.morrow@gmailcom>
2023-02-13 23:48:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/projecttwo/addItemToInventory.java
@@ -0,0 +1,14 @@
+package com.example.projecttwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addItemToInventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item_to_inventory);
+    }
+}
